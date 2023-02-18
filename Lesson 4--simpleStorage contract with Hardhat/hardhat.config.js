f2d0fb71e656043cd22ead9da7854fd2a9457a64
Haseeeb1 <haseebhassan701@gmail.com>
2023-02-18 13:02:27 +0500

    solidity

--- Lesson 4--simpleStorage contract with Hardhat/hardhat.config.js
+++ Lesson 4--simpleStorage contract with Hardhat/hardhat.config.js
@@ -5,13 +5,6 @@ require("./tasks/block-number")
 require("hardhat-gas-reporter")
 require("solidity-coverage")
 
-// task("accounts", "Prints the list of accounts", async () => {
-//     const accounts = await ethers.getSigners()
-
-//     for (const account of accounts) {
-//         console.log(account.address)
-//     }
-// })
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
