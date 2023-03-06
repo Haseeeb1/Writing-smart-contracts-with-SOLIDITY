3129e6939e7c266031d9da5109639367aa7c006c
Haseeeb1 <haseebhassan701@gmail.com>
2023-03-06 16:13:35 +0500

    t

--- Lesson 4--simpleStorage contract with Hardhat/hardhat.config.js
+++ Lesson 4--simpleStorage contract with Hardhat/hardhat.config.js
@@ -15,7 +15,7 @@ module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         goerli: { url: GOERLI_RPC_URL, accounts: [PRIVATE_KEY], chainId: 5 },
-        localhost: { url: "http://127.0.0.1:8545/", chainId: 31337 },
+        localhost: { url: "http://127.0.0.1:8545", chainId: 31337 },
     },
     solidity: "0.8.8",
     etherscan: {
